86bb9ce5dbf14ef979617a8717e126a4880050c9
Dandi <dandisaputra0122@gmail.com>
2024-06-14 07:54:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": "Poppins",
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        }
+      },
+    },
+  },
+  plugins: [],
+}
